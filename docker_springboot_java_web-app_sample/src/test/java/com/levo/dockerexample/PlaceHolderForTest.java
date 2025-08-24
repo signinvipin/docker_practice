1bb377d0cf1fa89d3b06f7a080ad04560ae59cbf
signinvipin <signinvipin@gmail.com>
2025-08-24 20:35:25 +0530

    Add files sample web_app

---
+++ docker_springboot_java_web-app_sample/src/test/java/com/levo/dockerexample/PlaceHolderForTest.java
@@ -0,0 +1,10 @@
+package com.levo.dockerexample;
+
+public class PlaceHolderForTest {
+    public static void main(String[] args) {
+        System.out.println("hello! This is a test app.\nCongratulations on setting up docker image successfully.\n\n" +
+            "/\\_/\\  \n" +
+            "( o.o ) \n" +
+            " > ^ < ");
+    }
+}
