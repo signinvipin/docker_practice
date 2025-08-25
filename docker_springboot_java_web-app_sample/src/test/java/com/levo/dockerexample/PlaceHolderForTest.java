ee9af32f6fdc024852b82f9663cf9e999a502f99
signinvipin <signinvipin@gmail.com>
2025-08-25 16:48:44 +0530

    Changed lines of code

--- docker_springboot_java_web-app_sample/src/test/java/com/levo/dockerexample/PlaceHolderForTest.java
+++ docker_springboot_java_web-app_sample/src/test/java/com/levo/dockerexample/PlaceHolderForTest.java
@@ -1,10 +1,27 @@
 package com.levo.dockerexample;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Spring Boot application entry point
+@SpringBootApplication
 public class PlaceHolderForTest {
     public static void main(String[] args) {
-        System.out.println("hello! This is a test app.\nCongratulations on setting up docker image successfully.\n\n" +
-            "/\\_/\\  \n" +
-            "( o.o ) \n" +
-            " > ^ < ");
+        SpringApplication.run(PlaceHolderForTest.class, args);
     }
 }
+
+// REST controller for "/" endpoint
+@RestController
+class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "hello! This is a test app.\nCongratulations on setting up docker image successfully.\n\n" +
+            "/\\_/\\  \n" +
+            "( o.o ) \n" +
+            " > ^ < ";
+    }
+}
+
